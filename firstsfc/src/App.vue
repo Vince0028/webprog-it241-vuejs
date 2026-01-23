b1813c115c96ab3d459506ecbd84ee6acc832c61
Vince0028 <Vince0028@users.noreply.github.com>
2026-01-23 08:56:18 +0800

    showing database and resume

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -15,7 +15,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <div class="app-container">
+    <div class="instruments-list">
+      <h2>Database Instruments</h2>
+      <ul>
+        <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+      </ul>
+    </div>
+    <resume-website />
+  </div>
 </template>
+
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+}
+.instruments-list {
+  padding: 20px;
+  background-color: #f0f0f0; 
+}
+</style>
+
