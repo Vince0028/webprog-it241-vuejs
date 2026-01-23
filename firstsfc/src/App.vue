5baedc343cb2f4712b09be03c03a39574212dcfe
Vince0028 <Vince0028@users.noreply.github.com>
2026-01-23 09:02:47 +0800

    database only

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,39 +1,17 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
-
 const instruments = ref([])
-
 async function getInstruments() {
   const { data } = await supabase.from('instruments').select()
   instruments.value = data
 }
-
 onMounted(() => {
    getInstruments()
 })
 </script>
-
 <template>
-  <div class="app-container">
-    <div class="instruments-list">
-      <h2>Database Instruments</h2>
-      <ul>
-        <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-      </ul>
-    </div>
-    <resume-website />
-  </div>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
-
-<style scoped>
-.app-container {
-  display: flex;
-  flex-direction: column;
-}
-.instruments-list {
-  padding: 20px;
-  background-color: #f0f0f0; 
-}
-</style>
-
