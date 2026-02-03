d90209bab55c16d184ac302b316203f0ce666a5c
Vince0028 <Vince0028@users.noreply.github.com>
2026-02-03 08:35:22 +0800

    Rest Api task 03

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <rest-api/>
+  <hr style="margin: 2rem 0; border: 0; border-top: 1px solid #ddd;" />
   <resume-website/>
   <h1>Instruments</h1>
   <InstrumentsList/>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -6,6 +6,7 @@ import InstrumentsList from './components/InstrumentsList.vue'
 import CommentsSection from './components/CommentsSection.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import RestApi from './components/RestApi.vue'
 
 const app = createApp(App)
 app.component('resume-website', ResumeWebsite)
@@ -13,4 +14,5 @@ app.component('InstrumentsList', InstrumentsList)
 app.component('comments-section', CommentsSection)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
+app.component('rest-api', RestApi)
 app.mount('#app')
